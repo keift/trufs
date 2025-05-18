dfb4a9148ca862695c456ffa8dcd642d8311b11a
Fırat Özden <fir4tozden@gmail.com>
2025-05-18 14:36:39 +0300

    v1.2.6

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,4 @@ import type { Fs } from "./types/main.type";
 export const fs: Fs = Object.assign({}, GracefulFS, FSExtra, Atomically);
 
 export type * as Types from "./types/main.type";
-<<<<<<< HEAD
 
-=======
->>>>>>> 6b78c43a91ddf99127c3aa88a38dfcbc3e83624f
