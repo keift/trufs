579dfdfedd1e1db1d58239631d4b6215968a00fb
Fırat Özden <fir4tozden@gmail.com>
2025-06-08 17:01:04 +0300

    v1.2.8

--- tests/Atomically/WriteFileSync.ts
+++ tests/Atomically/WriteFileSync.ts
@@ -1,7 +1,7 @@
 import { fs } from "../../src/main";
 
 const interval: NodeJS.Timeout = setInterval(() => {
-  fs.writeFileSync("./test/test.json", JSON.stringify({ updated_at: new Date().toISOString() }));
+  fs.writeFileSync("./tests/test.json", JSON.stringify({ updated_at: new Date().toISOString() }));
 }, 5);
 
 setTimeout(() => {
