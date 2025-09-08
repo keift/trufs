f47b9289eeb523d33ac3b49a72731bed4bbf47cd
Fırat Özden <fir4tozden@gmail.com>
2025-09-09 02:45:45 +0300

    v1.2.16

--- tests/FSExtra/Copy.test.ts
+++ tests/FSExtra/Copy.test.ts
@@ -1,5 +1,6 @@
 import { fs } from "../../src/main";
 
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 if (fs.copy === undefined) throw new Error("❌ Error");
 
 console.log("✅ Success");
--- tests/GracefulFS/Gracefulify.test.ts
+++ tests/GracefulFS/Gracefulify.test.ts
@@ -1,5 +1,6 @@
 import { fs } from "../../src/main";
 
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 if (fs.gracefulify === undefined) throw new Error("❌ Error");
 
 console.log("✅ Success");
