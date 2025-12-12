c551248371069bd7c979950dad80a5f08eb888d7
fir4tozden <fir4tozden@gmail.com>
2025-12-12 21:20:48 +0300

    v1.2.24

---
+++ tests/Dependencies.test.ts
@@ -0,0 +1,10 @@
+import Package from '../package.json';
+
+if (Package.dependencies['@types/fs-extra'] !== 'latest') throw new Error('❌ Error: @types/fs-extra');
+if (Package.dependencies['@types/graceful-fs'] !== 'latest') throw new Error('❌ Error: @types/graceful-fs');
+if (Package.dependencies['@types/node'] !== 'latest') throw new Error('❌ Error: @types/node');
+if (Package.dependencies.atomically !== 'latest') throw new Error('❌ Error: atomically');
+if (Package.dependencies['fs-extra'] !== 'latest') throw new Error('❌ Error: fs-extra');
+if (Package.dependencies['graceful-fs'] !== 'latest') throw new Error('❌ Error: graceful-fs');
+
+console.log('✅ Success');
