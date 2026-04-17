374df051cda15837060169c7a3c16ef5ab738e17
fir4tozden <fir4tozden@gmail.com>
2026-04-17 05:24:26 +0300

    v1.2.34

--- tests/Dependencies.test.ts
+++ tests/Dependencies.test.ts
@@ -8,6 +8,6 @@ if (Package.dependencies['graceful-fs'] !== 'latest') throw new Error('❌ Error
 
 if (Package.devDependencies.prettier !== 'latest') throw new Error('❌ Error: prettier');
 if (Package.devDependencies.rulint !== 'latest') throw new Error('❌ Error: rulint');
-if (Package.devDependencies.tsup !== 'latest') throw new Error('❌ Error: tsup');
+if (Package.devDependencies.tsdown !== 'latest') throw new Error('❌ Error: tsdown');
 
 console.log('✅ Success');
